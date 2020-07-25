3b0f2b97384cbe79c981df4b8cee2ee150b997ee
jaredhennessy <jared.hennessy@gmail.com>
2020-07-25 15:00:04 -0700

    Updates

--- src/components/Tech.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class Tech extends Component {
-  render() {
-    return <span className="tag is-info">{this.props.tech}</span>;
-  }
-}
-
-export default Tech;
